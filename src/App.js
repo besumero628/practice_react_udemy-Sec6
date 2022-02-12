4e053838ecf7ac10435ab5bd7dbcbf89feb31b13
besumero628 <70096064+besumero628@users.noreply.github.com>
2022-02-12 23:42:15 +0900

    6-4 Organism の作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/Searchinput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "test",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "1111@example.com",
+  phone: "000-1234-5678",
+  company: {
+    name: "Google"
+  },
+  website: "https://www.google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>Search</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
